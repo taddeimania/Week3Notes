ef4faef67f6bd9f07056a4237cd94232f16614b3
taddeimania <jtaddei@gmail.com>
2017-01-24 15:25:01 -0500

    Added SQL Server Project

---
+++ Day2Database/basic_selects.sql
@@ -0,0 +1,11 @@
+﻿-- This is a comment
+
+SELECT Band.name, Album.title, Album.release_date 
+FROM Album
+INNER JOIN Band ON Band.id = Album.band
+WHERE Album.title LIKE '%up%';
+
+SELECT Band.name, Album.title, Album.release_date 
+FROM Album
+INNER JOIN Band ON Band.id = Album.band
+WHERE Band.name = 'NOFX';
